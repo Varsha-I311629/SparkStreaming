a9cf7613fc52555f6f9e6486e77b42d6af7f851e
VarshaSreekumar3093 <81141753+VarshaSreekumar3093@users.noreply.github.com>
2021-03-24 14:05:02 +0530

    Update DynamoUtils.scala

--- src/main/scala/com.demovar.spark/streaming/storage/DynamoUtils.scala
+++ src/main/scala/com.demovar.spark/streaming/storage/DynamoUtils.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
+ * Copyright (c) 2015 Varsha Sreekumar. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
  * and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -10,7 +10,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package com.snowplowanalytics.spark.streaming
+package com.demovar.spark.streaming
 package storage
 
 // Java
@@ -20,7 +20,7 @@ import java.text.SimpleDateFormat
 
 
 // AWS Authentication
-// http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/auth/DefaultAWSCredentialsProviderChain.html
+
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 
 // AWS DynamoDB
